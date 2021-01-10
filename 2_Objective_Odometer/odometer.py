274ed79d2a93bb37c7328074b8625d1c89a7f9db
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2021-01-10 15:43:07 +0300

    Update odometer.py

--- 2_Objective_Odometer/odometer.py
+++ 2_Objective_Odometer/odometer.py
@@ -4,25 +4,19 @@ def odometer(oksana):
     time_massiv = []
     km = 0
 
-    # распределяем массивы по четности и нечетности индекса элемента массива
     for i in range(len(massiv)):
         if i % 2 == 0:
             chet_massiv.append(massiv[i])
         else:
             nechet_massiv.append(massiv[i])
 
-    # преобразовывем нечетный массив во временной
     for i in range(len(nechet_massiv)):
         if i == 0:
             time_massiv.append(nechet_massiv[i])
         else:
             time_massiv.append(nechet_massiv[i] - nechet_massiv[i-1])
 
-    # рассчитываем километраж
     for i in range(len(time_massiv)):
         km += chet_massiv[i] * time_massiv[i]
 
     return km
-
-massiv = [20, 2, 30, 6, 10, 7]
-print(odometer(massiv))
