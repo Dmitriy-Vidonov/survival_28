6515af6933a25372f811af64609c5507349a74b7
Dmitriy Vidonov <dmitriy.vidonov@gmail.com>
2021-01-19 07:40:00 +0300

    5_Objective_Salaries_Restore/chernovik.py

--- 5_Objective_Salaries_Restore/chernovik.py
+++ 5_Objective_Salaries_Restore/chernovik.py
@@ -1,8 +1,10 @@
-ids = [100, 50, 1, 1024, 200]
+ids = [50, 1, 1024]
 
 ids_used_els = []
+ids_used_coords = []
 
 ids_min_el = 0
+ids_min_el_coord = 0
 
 for k in range(len(ids)):
     # находим минимальный элемент в ids
@@ -20,9 +22,20 @@ for k in range(len(ids)):
 
     ids_used_els.append(ids_min_el)
 
+    # находим координаты минимального элемента в ids
+    for j in range(len(ids)):
+        if ids[j] == ids_min_el:
+            ids_min_el_coord = j
+    
+    ids_used_coords.append(ids_min_el_coord)
+
     print('ids_min_el =', ids_min_el)
     print('ids_used_els =', ids_used_els)
+    print('ids_min_el_coord =', ids_min_el_coord)
+    print('ids_used_coords =', ids_used_coords)
     print()
     print('************')
     print()
 
+# 1 - отсортировать по возрастанию список salary
+# 2 - в отсортированном salary переставить элементы по айдишкам из списка ids_used_coords
