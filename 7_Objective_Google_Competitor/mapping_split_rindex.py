69cade07079ba9a0d712072fcca6520f6bac8ce5
Dmitriy Vidonov <dmitriy.vidonov@gmail.com>
2021-01-31 13:41:55 +0300

    nothing special, just updated a list of useful functions with rindex example

--- 7_Objective_Google_Competitor/mapping_split_rindex.py
+++ 7_Objective_Google_Competitor/mapping_split_rindex.py
@@ -6,3 +6,6 @@ stroka = 'алеунде цум гебурстагенпляц!'
 words = stroka.split() # преобразуем строку в массив слов
 lens = map(word_len, words) # возвращаем длину каждого слова из списка слов
 print(list(lens))
+
+last_probel_position = stroka.rindex(' ') # последнее положение пробела в строке - только для строк
+print(last_probel_position)
