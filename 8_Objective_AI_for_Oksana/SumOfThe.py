6a8d63d31536ceaf9a3af2297ae0d12a3d932f88
Dmitriy Vidonov <dmitriy.vidonov@gmail.com>
2021-02-03 09:00:35 +0300

    function is ready

--- 8_Objective_AI_for_Oksana/SumOfThe.py
+++ 8_Objective_AI_for_Oksana/SumOfThe.py
@@ -1,20 +1,23 @@
-digits = [100, -50, 10, -25, 90, -35, 90]
-backup = [100, -50, 10, -25, 90, -35, 90]
+def SumOfThe(N: int, data: int) -> int: 
 
-digit = 0
-sum = 0
+    digits = data
+    backup = data
 
-for x in range(len(digits)):
-    digits = []
-    for z in range(len(backup)):
-        digits.append(backup[z])
+    digit = 0
     sum = 0
-    digit = digits[x]
-    digits.pop(x)
-    for y in range(len(digits)):
-        sum += digits[y]
-    if sum == digit:
-        print('сумма всех чисел массива =', digit)
-        break
-    else:
-        continue
+
+    for x in range(len(digits)):
+        digits = []
+        for z in range(len(backup)):
+            digits.append(backup[z])
+        sum = 0
+        digit = digits[x]
+        digits.pop(x)
+        for y in range(len(digits)):
+            sum += digits[y]
+        if sum == digit:
+            break
+        else:
+            continue
+
+    return digit
